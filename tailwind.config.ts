62ccde20d24ed15575e92ec3752629c2ae96e6d2
Arun Verma <arunverma759954@gmail.com>
2026-04-07 12:02:23 +0530

    fix config file

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
+      animation: {
+        'shine': 'shine 1.5s infinite',
+      },
+      keyframes: {
+        shine: {
+          '100%': { left: '125%' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
